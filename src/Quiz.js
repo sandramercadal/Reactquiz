c45bd2e759c8da629491aa42aba7108b22cb563e
sandramerc <144231118+sandramercadal@users.noreply.github.com>
2025-09-16 17:07:16 +0100

    Upadted the quiz event listeners

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ export default function Quiz(props) {
       scrambled: "LWOH",
       hint: "🏰 Can you unscramble the name of the wizard who lives here?",
       success:
-        "That's right! :) \n\nHowl is a powerful wizard known for his beautiful hair and dramatic personality!",
+        "That's right! Howl is a powerful wizard known for his beautiful hair and dramatic personality!",
       image:
         "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/167/896/original/howls-moving-castle.gif?1749652852",
     },
@@ -19,7 +19,7 @@ export default function Quiz(props) {
       scrambled: "YNOOP",
       hint: "🐠 Can you unscramble this girl's name?",
       success:
-        "That's right! :) \n\nPonyo is a magical fish girl who dreams of becoming human! The film won Animation of the Year and Outstanding Achievement in Music at the 32nd Japan Academy Prize.",
+        "That's right! Ponyo is a magical fish girl who dreams of becoming human! The film won Animation of the Year and Outstanding Achievement in Music at the 32nd Japan Academy Prize.",
       image:
         "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/167/782/original/ponyo-icegif-4.gif?1749581311",
     },
@@ -29,7 +29,7 @@ export default function Quiz(props) {
       scrambled: "IKIK",
       hint: "🐈‍⬛Can you guess this character's name?",
       success:
-        "Super! :) \n\nKiki is a young witch who starts her own delivery service! The book has won many awards including the Hans Christian Anderson Award for Writing.",
+        "Super! Kiki is a young witch who starts her own delivery service! The book has won many awards including the Hans Christian Anderson Award for Writing.",
       image:
         "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/167/900/original/kiki.gif?1749653660",
     },
@@ -39,7 +39,7 @@ export default function Quiz(props) {
       scrambled: "YTTEITRA",
       hint: "🏠 Can you unscramble this tiny character's name?",
       success:
-        "That's right! :) \n\nArrietty is a tiny 'borrower' who lives under the floorboards! The film won Animation of the year twice in 2011.",
+        "That's right! Arrietty is a tiny 'borrower' who lives under the floorboards! The film won Animation of the year twice in 2011.",
       image:
         "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/167/897/original/arrietty-ghibli.gif?1749653332",
     },
@@ -49,7 +49,7 @@ export default function Quiz(props) {
       scrambled: "RORTOTO",
       hint: "🐻 Can you unscramble the name of this gentle creature?",
       success:
-        "That's right! :) \n\nTotoro is a forest spirit who is befriended by sisters Satsuki and Mei. My Neighbour Totoro, celebrated for its enchanting animation has won numerous awards for best film!",
+        "That's right! Totoro is a forest spirit who is befriended by sisters Satsuki and Mei. My Neighbour Totoro, celebrated for its enchanting animation has won numerous awards for best film!",
       image:
         "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/167/906/original/totoro2.gif?1749654760",
     },
@@ -59,19 +59,21 @@ export default function Quiz(props) {
       scrambled: "HIROCHI",
       hint: "⽰ What's the name of this girl who helps her parents when they are turned into pigs?",
       success:
-        "That's right! :) \n\nIn 'Spirited Away' the ghost who helps Chihiro is called No-Face also known as Koanashi in Japanese which translates to face-less. He is a spirit who reacts to emotions and absorbs the personalities of those he consumes. The film won an Academy Award for Animated Feature and is described as the best Ghibli film of all time.",
+        "That's right! In 'Spirited Away' the ghost who helps Chihiro is called No-Face also known as Koanashi in Japanese which translates to face-less. He is a spirit who reacts to emotions and absorbs the personalities of those he consumes. The film won an Academy Award for Animated Feature and is described as the best Ghibli film of all time.",
       image:
         "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/167/902/original/spirited-away2.gif?1749654030",
     },
   ];
 
   function handleCharacterClick(character) {
-    const userAnswer = prompt(`${character.hint}\n\n${character.scrambled}`);
+    const userAnswer = prompt(character.hint + " " + character.scrambled);
 
-    if (
-      userAnswer &&
-      userAnswer.toLowerCase().trim() === character.name.toLowerCase()
-    ) {
+    if (!userAnswer || userAnswer.trim() === "") {
+      alert("Sorry that's not quite right, have another go!");
+      return;
+    }
+
+    if (userAnswer.toLowerCase().trim() === character.name.toLowerCase()) {
       alert(character.success);
     } else {
       alert("Not quite, why don't you have another go!");
@@ -86,32 +88,64 @@ export default function Quiz(props) {
       <h3>Click to unscramble and discover!</h3>
 
       <div className="row">
-        {characters.slice(0, 3).map((character) => (
-          <div key={character.id} className="col-md-4 mt-1">
-            <img
-              className="pic"
-              src={character.image}
-              onClick={() => handleCharacterClick(character)}
-              alt={character.name}
-              style={{ cursor: "pointer" }}
-            />
-          </div>
-        ))}
+        <div className="col-md-4 mt-1">
+          <img
+            className="pic"
+            src={characters[0].image}
+            onClick={() => handleCharacterClick(characters[0])}
+            alt={characters[0].name}
+            style={{ cursor: "pointer" }}
+          />
+        </div>
+        <div className="col-md-4 mt-1">
+          <img
+            className="pic"
+            src={characters[1].image}
+            onClick={() => handleCharacterClick(characters[1])}
+            alt={characters[1].name}
+            style={{ cursor: "pointer" }}
+          />
+        </div>
+        <div className="col-md-4 mt-1">
+          <img
+            className="pic"
+            src={characters[2].image}
+            onClick={() => handleCharacterClick(characters[2])}
+            alt={characters[2].name}
+            style={{ cursor: "pointer" }}
+          />
+        </div>
       </div>
 
       <div className="card-container2 mt-4">
         <div className="row">
-          {characters.slice(3, 6).map((character) => (
-            <div key={character.id} className="col-md-4 mt-1">
-              <img
-                className="pic"
-                src={character.image}
-                onClick={() => handleCharacterClick(character)}
-                alt={character.name}
-                style={{ cursor: "pointer" }}
-              />
-            </div>
-          ))}
+          <div className="col-md-4 mt-1">
+            <img
+              className="pic"
+              src={characters[3].image}
+              onClick={() => handleCharacterClick(characters[3])}
+              alt={characters[3].name}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+          <div className="col-md-4 mt-1">
+            <img
+              className="pic"
+              src={characters[4].image}
+              onClick={() => handleCharacterClick(characters[4])}
+              alt={characters[4].name}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+          <div className="col-md-4 mt-1">
+            <img
+              className="pic"
+              src={characters[5].image}
+              onClick={() => handleCharacterClick(characters[5])}
+              alt={characters[5].name}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
         </div>
       </div>
     </div>
